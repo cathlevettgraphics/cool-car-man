c4eda46bcb07f73c5ea8d6d5b6e268b57b839ece
cath levett <cathlevettgraphics@gmail.com>
2021-02-18 07:57:19 +0000

    added descriptive names

--- js/crud.js
+++ js/crud.js
@@ -1,31 +1,30 @@
-import { postsMountNode } from "./appController.js";
-import { testData } from "./appController.js";
+import { postsMountNode, CARS_ENDPOINT } from './appController.js';
 
 export let allCars = [];
 
 // Fetch
-export async function fetchData(dataToFetch) {
+export async function fetchData(API_ENDPOINT) {
   try {
-    const res = await fetch(dataToFetch);
+    const res = await fetch(API_ENDPOINT);
     const data = await res.json();
 
     allCars = data;
-    console.log("1. original posts from server", allCars);
+    // console.log('1. original posts from server', allCars);
     renderCarList(allCars);
   } catch (err) {
-    console.log("error:", err.message);
+    console.log('error:', err.message);
   }
 }
 
 // Render
 export function renderCarList() {
   if (allCars.length) {
-    const list = document.createElement("ul");
-    list.classList.add("car-list");
+    const list = document.createElement('ul');
+    list.classList.add('car-list');
 
     for (const { name, bhp, avatar_url, _id } of allCars) {
-      const li = document.createElement("li");
-      li.classList.add("car-item");
+      const li = document.createElement('li');
+      li.classList.add('car-item');
       li.innerHTML = `
       <div class="car-wrapper">
       <h3>${name}</h3>
@@ -39,7 +38,7 @@ export function renderCarList() {
       `;
       list.append(li);
     }
-    postsMountNode.innerHTML = "";
+    postsMountNode.innerHTML = '';
     postsMountNode.append(list);
   } else {
     postsMountNode.innerHTML = ` <p class="no-data-entered">No cars added – what are you waiting for! Add some cool cars</p>`;
@@ -47,13 +46,13 @@ export function renderCarList() {
 }
 
 // Create
-export async function postData(data) {
+export async function addCar(data) {
   try {
-    const response = await fetch(testData, {
-      method: "POST",
+    const response = await fetch(CARS_ENDPOINT, {
+      method: 'POST',
       body: JSON.stringify(data),
       headers: {
-        "Content-type": "application/json; charset=UTF-8",
+        'Content-type': 'application/json; charset=UTF-8',
       },
     });
     if (!response.ok) {
@@ -64,38 +63,38 @@ export async function postData(data) {
     allCars.push(newCarData);
     renderCarList(allCars);
   } catch (err) {
-    console.log("err", err.message);
+    console.log('err', err.message);
   }
 }
 
 // Update
-export async function updateData(carId, changes) {
-  let itemToUpdate = `${testData}${carId}`;
+export async function updateCar(carId, changes) {
+  let itemToUpdate = `${CARS_ENDPOINT}${carId}`;
 
   try {
     const res = await fetch(itemToUpdate, {
-      method: "PUT",
+      method: 'PUT',
       body: JSON.stringify(changes),
       headers: {
-        "Content-type": "application/json; charset=UTF-8",
+        'Content-type': 'application/json; charset=UTF-8',
       },
     });
-    fetchData(testData);
+    fetchData(CARS_ENDPOINT);
   } catch (err) {
-    console.log("error:", err.message);
+    console.log('error:', err.message);
   }
 }
 
 // Delete
-export async function deleteData(idToDelete) {
-  const itemToDelete = `${testData}${idToDelete}`;
+export async function deleteCar(idToDelete) {
+  const itemToDelete = `${CARS_ENDPOINT}${idToDelete}`;
 
   try {
     const res = await fetch(itemToDelete, {
-      method: "DELETE",
+      method: 'DELETE',
     });
-    fetchData(testData);
+    fetchData(CARS_ENDPOINT);
   } catch (err) {
-    console.log("error", err, message);
+    console.log('error', err, message);
   }
 }
